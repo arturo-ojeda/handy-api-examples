bce2e5c87022360587dd93f93f7596139c9d107e
Arturo Ojeda <arturo@handy.la>
2022-01-05 16:45:08 +0100

    Added JavaScript code sample

---
+++ javascript/README.md
@@ -0,0 +1,1 @@
+Use the command `node index.js` to run the process.
---
+++ javascript/index.js
@@ -0,0 +1,104 @@
+/**
+ * Sample code to fetch sales orders from Handy API.
+ * Run the code with `node index.js`
+ * 
+ * You only need to implement your business logic in the businessLogic function.
+ * 
+ * This code handles the API:
+ * - Rate limit
+ * - Pagination
+ * - Authentication
+ * - Errors
+ * 
+ * Note:
+ * 1. You need to set the HANDY_BEARER_TOKEN environment variable to your Handy API bearer token.
+ * 2. You can adjust the cron expression to run the code at a different time.
+ * 
+ */
+
+// https://crontab.guru/#*/10_*_*_*_*
+// Default: every 10 minutes
+const cronExpression = '*/10 * * * *'
+
+const businessLogic = async function (salesOrders) {
+    // Implement your business logic here:
+    console.log(salesOrders);
+};
+
+// -----------------------------------------------------------------------------
+
+require('dotenv').config()
+const fetch = require('node-fetch');
+const CronJob = require('cron').CronJob;
+const fs = require('fs');
+const dateFormat = 'DD/MM/YYYY HH:mm:ss'
+const moment = require('moment');
+
+const fetchSalesOrders = async () => {
+    let lastTime;
+    const filePath = './last_time.json';
+    if (fs.existsSync(filePath)) {
+        lastTime = JSON.parse(fs.readFileSync(filePath));
+        lastTime.start = lastTime.end
+        lastTime.end = moment().format(dateFormat);
+    } else {
+        // Defaults
+        lastTime = {
+            start: moment().format(dateFormat),
+            end: moment().format(dateFormat)
+        }
+    }
+
+    const url = `https://app.handy.la/api/v2/salesOrder?start=${lastTime.start}&end=${lastTime.end}`;
+    const response = await queryHandyAPI(url);
+
+    if (!response) {
+        console.log('No response from Handy API');
+        return
+    }
+
+    const salesOrders = response.salesOrders
+
+    while (response && response.pagination.nextPage) {
+        response = await queryHandyAPI(response.pagination.nextPage);
+        salesOrders.push(...response.salesOrders);
+    }
+
+    try {
+        await businessLogic(salesOrders);
+    } catch (e) {
+        console.error("Error implementing business logic", e);
+    }
+
+    fs.writeFileSync('lastTime.json', JSON.stringify(lastTime));
+};
+
+const queryHandyAPI = async function (url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${process.env.HANDY_BEARER_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.status === 429) {
+        console.log('Rate limit reached. Waiting for 60 seconds.');
+        await sleep(60000);
+        return queryHandyAPI(url);
+    }
+
+    if (response.status === 200) {
+        return response.json();
+    } else {
+        console.log('Error: ' + response.status);
+        return null
+    }
+};
+
+const sleep = function (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+new CronJob(cronExpression, fetchSalesOrders, null, true, 'America/Mexico_City', this, true);
+console.log("Started cron job");
