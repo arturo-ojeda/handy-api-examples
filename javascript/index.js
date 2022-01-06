57997969780aa0e03bfd2966a9003dc0e6e30d25
Arturo Ojeda <arturo@handy.la>
2022-01-06 17:53:41 +0100

    Added support for deleted sales orders

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 /**
  * Sample code to fetch sales orders from Handy API.
+ * It can query created and deleted sales orders.
  * Run the code with `node index.js`
  * 
  * You only need to implement your business logic in the businessLogic function.
@@ -12,7 +13,8 @@
  * 
  * Note:
  * 1. You need to set the HANDY_BEARER_TOKEN environment variable to your Handy API bearer token.
- * 2. You can adjust the cron expression to run the code at a different time.
+ *    You can either create a .env file or add it as an environment variable in your operating system.
+ * 2. You can adjust the cron expression to run the code at a frequency.
  * 
  */
 
@@ -20,12 +22,27 @@
 // Default: every 10 minutes
 const cronExpression = '*/10 * * * *'
 
-const businessLogic = async function (salesOrders) {
-    // Implement your business logic here:
-    console.log(`Received ${salesOrders.length} sales orders`);
-    salesOrders.forEach(salesOrder => {
-        // console.log(salesOrder.id);
-    })
+const businessLogic = async function (salesOrders, deleted) {
+    console.log(`${deleted ? 'Fetched deleted orders' : 'Fetched created orders'}. Received ${salesOrders.length} sales orders.`);
+
+    for (const salesOrder of salesOrders) {
+        if (deleted) {
+            // A sales order was deleted in Handy
+            // If you want, you can delete the externalId from your system.
+        } else {
+            // *****
+            // Implement your business logic here:
+            // ✅ Save sales order in your ERP or system.
+            // *****
+
+            // Once you saved the sales order in your system, 
+            // you can save the externalId on the sales order with the Handy API
+            // for future reference if the sales order is deleted in Handy.
+            // Then you can easily find the corresponding sales order in your system
+            // and delete it too.
+            await saveExternalIdOnSalesOrder(salesOrder.id, salesOrder.externalId);
+        }
+    }
 };
 
 // -----------------------------------------------------------------------------
@@ -37,9 +54,14 @@ const fs = require('fs');
 const dateFormat = 'DD/MM/YYYY HH:mm:ss'
 const moment = require('moment');
 
-const fetchSalesOrders = async () => {
+const jobFunction = async function () {
+    await fetchSalesOrders(false);
+    await fetchSalesOrders(true);
+}
+
+const fetchSalesOrders = async (deleted) => {
     let lastTime = {};
-    const filePath = './last_time.json';
+    const filePath = deleted ? './last_time_deleted.json' : './last_time.json';
 
     if (fs.existsSync(filePath)) {
         lastTime.start = JSON.parse(fs.readFileSync(filePath));
@@ -50,7 +72,7 @@ const fetchSalesOrders = async () => {
         lastTime.end = lastTime.start;
     }
 
-    const url = `https://app.handy.la/api/v2/salesOrder?start=${lastTime.start}&end=${lastTime.end}`;
+    const url = `https://app.handy.la/api/v2/salesOrder?start=${lastTime.start}&end=${lastTime.end}&deleted=${deleted}`;
     let response = await queryHandyAPI(url);
 
     if (!response) {
@@ -66,12 +88,12 @@ const fetchSalesOrders = async () => {
     }
 
     try {
-        await businessLogic(salesOrders);
+        await businessLogic(salesOrders, deleted);
     } catch (e) {
         console.error("Error implementing business logic", e);
     }
 
-    fs.writeFileSync('last_time.json', JSON.stringify(lastTime.end));
+    fs.writeFileSync(filePath, JSON.stringify(lastTime.end));
 };
 
 const queryHandyAPI = async function (url) {
@@ -97,9 +119,27 @@ const queryHandyAPI = async function (url) {
     }
 };
 
+const saveExternalIdOnSalesOrder = async function (salesOrderId, externalId) {
+    const url = `https://app.handy.la/api/v2/salesOrder/${salesOrderId}`;
+    const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${process.env.HANDY_BEARER_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ externalId })
+    });
+
+    if (response.status === 200) {
+        console.log(`Saved externalId ${externalId} on sales order ${salesOrderId}`);
+    } else {
+        console.log(`Error saving externalId ${externalId} on sales order ${salesOrderId}`);
+    }
+}
+
 const sleep = function (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-new CronJob(cronExpression, fetchSalesOrders, null, true, 'America/Mexico_City', this, true);
+new CronJob(cronExpression, jobFunction, null, true, 'America/Mexico_City', this, true);
 console.log("Started cron job");
